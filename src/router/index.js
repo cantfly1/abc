9ca95e78109e0d9be258ae5e5ad6aa32bdc3d11f
cantfly1 <ines.roumier@epitech.digital>
2021-11-07 04:17:06 +0100

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,17 @@
 import { createWebHistory, createRouter } from "vue-router";
 import Login from "@/views/Login.vue";
 import register from "@/views/Profile.vue";
-
+import test from "@/views/test.vue";
 const routes = [
   { 
     name: 'login',
     path: '/login', 
     component: Login,
+  },
+  { 
+    name: 'test',
+    path: '/', 
+    component: test,
   },
   { 
     name: 'register',
---
+++ src/views/test.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>H</h1>
+</template>
+<script>
+var express = require('express');
+var app = express();
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('http://localhost:8081/', function(req, res) {
+  res.send('hello world');
+});
+
+
+</script>
